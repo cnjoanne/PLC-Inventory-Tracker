c8f34948f6f2e461fbc3aee12681e2b6344d83e2
cnjoanne <scn153@gmail.com>
2025-03-04 16:39:35 +0800

    prepping

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+/* TODO: process input csv */
+
+/* TODO: create struct of item: item_name; quantity; expiry_date 
+typedef struct{
+    char *item_name;
+    int quantity;
+    char expiry_date[11]; 
+} Item; */
+
+/* TODO: create array (pointer to array) to store many items */
+
+/* TODO: convert data into binary, and store in cache folder*/
+
+/* TODO: process user's commands, eg. sort item, filter, export */
+
+/* TODO: read binary cache */
+
+/* TODO: sorting logic for item_name, quantity, expiry_date */
+
+/* TODO: filter logic for low quantity, expiry_date */
+
+/* TODO: write binary, overwrite previous binary cache */
+
+/* TODO: write binary cache into LaTeX */
+
+/* TODO: export LaTeX as output in ./data */
+
 int main(){
 
 

7ebfc3cef83a9f97bd1589e3c9bd9123796ac647
ngzhenghan <ngzhenghanzh@gmail.com>
2025-03-21 19:29:02 +0800

    Test Function for latex export function

--- src/latex_export.c
+++ src/latex_export.c
@@ -2,3 +2,7 @@
 /* TODO: export LaTeX as output in ./data */
 /* Input: array(pointer of array) of items */
 /* Saves LaTeX file in ./data/output.tex */
+
+void to_latex(const void *input_low_stock, const void *input_expiring_stock, const void *input_sorted_stock) {
+    
+}

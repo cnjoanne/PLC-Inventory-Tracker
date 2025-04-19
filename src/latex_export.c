2b2b45a25f225fb0a56f753b1259f8d9cfbbcf0a
cnjoanne <scn153@gmail.com>
2025-04-19 21:08:14 +0800

    updated latex

--- src/latex_export.c
+++ src/latex_export.c
@@ -1,8 +1,3 @@
-
-/* TODO: export LaTeX as output in ./data */
-/* Input: array(pointer of array) of items */
-/* Saves LaTeX file in ./data/output.tex */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "latex_export.h"
@@ -17,24 +12,21 @@ void write_table(FILE *file, Item **items, int num_rows, char* title)
     int i;
 
     fprintf(file, "\\section*{%s}\n", title);
-    fprintf(file, "\\noindent\\hspace*{-\\oddsidemargin}");
-    fprintf(file, "\\begin{tabular}{|p{0.4\\linewidth}|p{0.3\\linewidth}|p{0.3\\linewidth}|}\n");
+    fprintf(file, "\\begin{longtable}{|p{0.4\\linewidth}|p{0.25\\linewidth}|p{0.25\\linewidth}|}");
     fprintf(file, "\\hline\n");
     fprintf(file, "\\textbf{Name} & \\textbf{Quantity} & \\textbf{Expiry Date}");
     fprintf(file, "%s\n", "\\\\");
     fprintf(file, "\\hline\n");
 
-
     for (i = 0; i < num_rows; i++)
     {
         fprintf(file, "%s & %d & %s %s\n", items[i]->item_name, items[i]->quantity, items[i]->expiry_date, "\\\\");
-        printf("listing table rows....\n");
+        /*printf("listing table rows....\n"); 
         printf("%s & %d & %s \n", items[i]->item_name, items[i]->quantity, items[i]->expiry_date);
-        printf("end of table rows....\n");
+        printf("end of table rows....\n"); */
         fprintf(file, "\\hline\n");
     }
-
-    fprintf(file, "\\end{tabular}\n\n");
+   fprintf(file, "\\end{longtable}\n\n"); 
 }
 
 void to_latex(Item** input_low_stock, Item** input_expiring_stock, Item** input_sorted_stock,
@@ -56,7 +48,20 @@ void to_latex(Item** input_low_stock, Item** input_expiring_stock, Item** input_
             return;
         }
 
-    fprintf(file, "\\documentclass{article}\n\\begin{document}\n\\vspace*{-1in}\n");
+    fprintf(file, "\\documentclass{article}\n");
+
+    /* packages */
+    fprintf(file, "\\usepackage[margin=1in]{geometry}\n\\usepackage{longtable}\n\\usepackage{datetime}\n\\usepackage{fancyhdr}\n"); 
+
+    /* pages */
+    fprintf(file, "\\pagestyle{fancy}\n\\fancyfoot[C]{}\n\\fancyfoot[L]{}\n\\fancyfoot[R]{\\thepage}\n\\renewcommand{\\headrulewidth}{0pt}");
+
+    fprintf(file, "\\begin{document}\n");
+
+    /* Title and date */
+    fprintf(file, "\\noindent\n\\begin{minipage}[t]{0.48\\textwidth}\n\t\\raggedright\n\t\\LARGE \\textbf{Stock Report}\n\\end{minipage}\n");
+    fprintf(file, "\\begin{minipage}[t]{0.48\\textwidth}\n\t\\raggedleft\n\t\\large \\today\n\\end{minipage}\n");
+    fprintf(file, "\\vspace{1.5em}\n");
 
     write_table(file, input_low_stock, num_low_stock, "Low Stock Items");
     write_table(file, input_expiring_stock, num_expiring_stock, "Expiring Stock Items");
